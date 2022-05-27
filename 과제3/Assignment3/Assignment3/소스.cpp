fdb4f7d59257bdedd27a05e5fb0318eeafc59d0d
yeongchankim <b611040@g.hongik.ac.kr>
2022-05-28 04:15:54 +0900

    폴더3추가

---
+++ 과제3/Assignment3/Assignment3/소스.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<fstream>
+#include<string.h>
+using namespace std;
+
+#define MAX_STRING 32
+#define INPUT_FILE_NAME "input.txt"
+#define OUTPUT_FILE_NAME "output.txt"
+
+void doTask();
+void join();
+void program_exit();
+
+//FILE* in_fp, * out_fp;
+
+int main() 
+{
+	//FILE* in_fp = fopen(INPUT_FILE_NAME, "r+");
+	//FILE* out_fp = fopen(OUTPUT_FILE_NAME, "w+");
+
+
+}
